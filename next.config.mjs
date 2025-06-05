57cce9214bc2f2517d670a88abfa2f96fdbf1ff4
dotplato <iamdotplato@gmail.com>
2025-06-05 17:04:15 +0500

    next redirects added

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -9,6 +10,33 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/time-poverty-in-a-fast-world-reclaiming-life-through-the-slow-movement',
+        destination: '/library/time-poverty-in-a-fast-world-reclaiming-life-through-the-slow-movement',
+        permanent: true,
+      },
+      {
+        source: '/effective-accelerationism-eacc-and-how-you-can-be-a-part-of-it',
+        destination: '/library/effective-accelerationism-eacc-and-how-you-can-be-a-part-of-it',
+        permanent: true,
+      },
+{
+        source: '/from-conformity-to-curiosity-why-pakistan-must-embrace-philosophy',
+        destination: '/library/from-conformity-to-curiosity-why-pakistan-must-embrace-philosophy',
+        permanent: true,
+      },
+
+      {
+        source: '/can-ai-achieve-human-morality',
+        destination: '/library/can-ai-achieve-human-morality',
+        permanent: true,
+      },
+          ]
+  },
 }
 
 export default nextConfig
+
